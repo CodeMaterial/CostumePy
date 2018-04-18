bfdb65001aae493d8afc0210ba796f860afb0cd1
Sam <martinsa0182@btinternet.com>
2018-04-18 09:48:13 +0100

    Adjusted refresh rates and removed overrunning attribute
    Signed-off-by: Sam <martinsa0182@btinternet.com>

--- system/costume_modules.py
+++ system/costume_modules.py
@@ -3,26 +3,29 @@ import time
 import logging
 from system.events import Event
 
+
 class CostumeModule(Process):
 
-    def __init__(self, refresh_rate=1, ignore_overrun=False):
+    def __init__(self, refresh_rate=1):
         super().__init__()
         self.refresh_rate = refresh_rate
+        self.name = self.__class__.__name__
+        self.description = None
+
         self.running = True
         self.next_loop = None
-        self.ignore_overrun = ignore_overrun
         self.is_setup = False
 
         self.send_queue = None
         self.receive_queue = None
 
-        self.name = self.__class__.__name__
-
-        self.listeners = {"SHUTDOWN": self.shutdown, "UI_REQUEST": self.ui_request}
-
-        self.description = None
-
+        self.listeners = {"SHUTDOWN": self.shutdown,
+                          "UI_REQUEST": self.ui_request,
+                          "MEASURE_REFRESH": self.measure_refresh}
         self.actions = {}
+
+        self.refresh_times_limit = int(max(2.0, 1.0/refresh_rate*2))
+        self.refresh_times = [0]*self.refresh_times_limit
 
         logging.info("Initialised")
 
@@ -36,10 +39,21 @@ class CostumeModule(Process):
     def setup(self):
         pass
 
-    def ui_request(self, event):
-        e = Event("UI", {"descr": self.description, "actions": self.actions})
+    def measure_refresh(self, event):
 
-        self.broadcast("UI")
+        rate = 0
+        if len(self.refresh_times) == self.refresh_times_limit:
+
+            time_sum = 0
+            for i in range(self.refresh_times_limit-1):
+                time_sum += self.refresh_times[i+1] - self.refresh_times[i]
+
+            rate = time_sum/(self.refresh_times_limit-1)
+
+        self.broadcast("REFRESH_RATE", data=rate)
+
+    def ui_request(self, event):
+        self.broadcast("UI", {"descr": self.description, "actions": self.actions})
 
     def set_queues(self, manager_queue, module_queue):
         logging.debug("Setting queues")
@@ -74,17 +88,19 @@ class CostumeModule(Process):
             self.is_setup = True
 
         self.execute_queue()
+
         current_time = time.time()
         if not self.next_loop:
             self.next_loop = current_time + self.refresh_rate
 
         time_to_sleep = self.next_loop - current_time
-        if time_to_sleep > 0:
-            time.sleep(self.refresh_rate)
-        else:
-            if not self.ignore_overrun:
-                logging.error("Overrunning by %r" % time_to_sleep)
 
         self.next_loop = current_time + self.refresh_rate
+
+        self.refresh_times.append(current_time)
+        self.refresh_times = self.refresh_times[1:self.refresh_times_limit+1]
+
+        if time_to_sleep > 0:
+            time.sleep(time_to_sleep)
 
         return self.running
