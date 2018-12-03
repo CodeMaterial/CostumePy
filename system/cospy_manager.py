ea9563c45ddee6c42678c753d5c1e813a774f6f4
Samuel Martin <martinsa0182@btinternet.com>
2018-12-03 09:27:18 +0000

    cleaned up manager code and fixed typos

--- system/cospy_manager.py
+++ system/cospy_manager.py
@@ -4,17 +4,19 @@ import zmq
 import threading
 import CostumePy
 
+
 class CospyManager:
 
     def __init__(self):
         self._node_sockets = {}
         self._listeners = {}
         CostumePy.set_logging_level(logging.DEBUG)
+        self._manager_listeners = {"_listen_for": self.register_listener}
 
         self.request_socket = zmq.Context().socket(zmq.REP)
         self.request_socket.bind("tcp://*:5556")
 
-        self.avaliable_ip = 5557
+        self.available_ip = 5557
 
         ip_address_manager = threading.Thread(target=self.manage_ip_requests)
         ip_address_manager.start()
@@ -23,57 +25,50 @@ class CospyManager:
 
         while True:
             node_name = self.request_socket.recv_string()
-            address = "tcp://localhost:%i" % self.avaliable_ip
+            address = "tcp://localhost:%i" % self.available_ip
             self.request_socket.send_string(address)
             soc = zmq.Context().socket(zmq.PAIR)
-            soc.bind("tcp://*:%i" % self.avaliable_ip)
+            soc.bind("tcp://*:%i" % self.available_ip)
             self._node_sockets[node_name] = soc
-            self.avaliable_ip += 1
+            self.available_ip += 1
 
-    def register_node(self, node_name, topic_to_listen):
-        logging.info("Registering  %s for %s" % (node_name, topic_to_listen))
+    def register_listener(self, msg):
+        topic, node_name = msg["data"], msg["source"]
+        logging.info("Registering  %s for %s" % (node_name, topic))
 
-        if topic_to_listen not in self._listeners:
-            self._listeners[topic_to_listen] = []
+        if topic not in self._listeners:
+            self._listeners[topic] = []
 
-        if node_name not in self._listeners[topic_to_listen]:
-            self._listeners[topic_to_listen].append(node_name)
+        if node_name not in self._listeners[topic]:
+            self._listeners[topic].append(node_name)
 
     def run(self):
 
         logging.info("Starting queue management")
 
-        msg_backlog = []
-
         while True:
-            messages = msg_backlog
-            msg_backlog = []
             for node_name in list(self._node_sockets):
                 try:
                     soc = self._node_sockets[node_name]
                     msg = soc.recv_json(flags=zmq.NOBLOCK)
-                    if msg["source"] is None:
-                        msg["source"] = node_name
-                    messages.append(msg)
+                    msg["source"] = node_name if msg["source"] is None else msg["source"]
+
+                    if msg["action_at"] <= time.time():
+                        logging.info("Received message %r" % msg)
+
+                        topic = msg["topic"]
+
+                        if topic in self._manager_listeners:
+                            self._manager_listeners[topic](msg)
+                        else:
+                            if topic in self._listeners:
+                                for nodes_listening in self._listeners[topic]:
+                                    logging.info("Sending %r to %s" % (msg, nodes_listening))
+                                    self._node_sockets[nodes_listening].send_json(msg)
+                            else:
+                                logging.info("No one listening to %s" % msg)
                 except zmq.Again:
                     pass
-
-            for msg in messages:
-
-                if msg["action_at"] > time.time():
-                    msg_backlog.append(msg)
-                else:
-                    logging.info("Received message %r" % msg)
-
-                    if msg["topic"] == "_listen_for":
-                        self.register_node(msg["source"], msg["data"])
-                    else:
-                        if msg["topic"] in self._listeners:
-                            for node_name in self._listeners[msg["topic"]]:
-                                logging.info("Sending %r to %s" % (msg, node_name))
-                                self._node_sockets[node_name].send_json(msg)
-                        else:
-                            logging.info("No one listening to %s" % msg)
 
 
 if __name__ == "__main__":
