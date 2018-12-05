3d9ac1d1c8aa5d78b44215518ea6461d241fddf4
Samuel Martin <sam@codematerial.com>
2018-12-05 22:55:01 +0000

    Altered round trip speed test. Need to implement logging levels from command line to optimise server

--- tests/round_trip_speed.py
+++ tests/round_trip_speed.py
@@ -1,18 +1,26 @@
 import CostumePy
 import time
 import random
-
+import sys
 from callback_helpers import CallbackReceiver
 
 cr = CallbackReceiver()
+if len(sys.argv) != 3:
+	print("Please enter the number of random float values per message and the total number of messages as arguments")
+	quit()
 
-test_data = [random.random() for _ in range(3*1000)]
+items = int(sys.argv[1])
+loops = int(sys.argv[2])
+
+test_data = [random.random() for _ in range(items)]
 
 CostumePy.listen_to("ping", cr.func)
 total = 0
-for _ in range(1000):
+for _ in range(loops):
     CostumePy.broadcast("ping", data=test_data)
     msg = cr.get_received()
     total += (time.time() - msg["created"])
 
-print("round trip time per message of 3000 float values: %f seconds" % total/1000.0)
+print("Round trip time to server and back per message of %i float values: %f seconds (%i fps)" % (items, total/loops, int(loops/total)))
+
+CostumePy.stop()
